da4c26a018d11cc0abed0f1046876ccc30d6276f
Виталий Епанча <vitaliy@Vitalijs-MacBook-Pro.local>
2021-04-05 19:49:33 +0300

    Lesson 6 ex 10

--- src/lib/TestCore.java
+++ src/lib/TestCore.java
@@ -1,6 +1,7 @@
 package lib;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -9,6 +10,7 @@ import java.net.URL;
 
 public class TestCore extends TestCase {
 
+    private static final String PLATFORM = System.getenv("PLATFORM");
     protected AppiumDriver driver;
     private static String appiumURL = "http://127.0.0.1:4723/wd/hub";
 
@@ -16,9 +18,11 @@ public class TestCore extends TestCase {
     protected void setUp() throws Exception {
         super.setUp();
         DesiredCapabilities desiredCapabilities = desiredCapabilities();
-    //    driver = new AndroidDriver(new URL(appiumURL), desiredCapabilities);
-        driver = new IOSDriver(new URL(appiumURL), desiredCapabilities);
-     //   driver.rotate(ScreenOrientation.PORTRAIT);
+        if (PLATFORM.equals("ios")) {
+            driver = new IOSDriver(new URL(appiumURL), desiredCapabilities);
+        } else if (PLATFORM.equals("Android")) {
+            driver = new AndroidDriver(new URL(appiumURL), desiredCapabilities);
+        } else throw new Exception("Cannot get run platform from system environment " + PLATFORM);
     }
 
     @Override
@@ -28,10 +32,10 @@ public class TestCore extends TestCase {
     }
 
     private DesiredCapabilities desiredCapabilities() throws Exception{
-      //  String platform = System.getenv("PLATFORM");
-        String platform = "ios";
+    //    String platform = System.getenv("PLATFORM");
+        System.out.println(PLATFORM);
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        if (platform.equals("android")) {
+        if (PLATFORM.equals("android")) {
             capabilities.setCapability("platformName", "Android");
             capabilities.setCapability("deviceName", "AndroidTestDevice");
             capabilities.setCapability("platformVersion", "8.1");
@@ -39,13 +43,13 @@ public class TestCore extends TestCase {
             capabilities.setCapability("appPackage", "org.wikipedia");
             capabilities.setCapability("appActivity", ".main.MainActivity");
             capabilities.setCapability("app", "/Users/vitaliy/Documents/IdeaProjects/JavaAppiumAutomation/apks/org.wikipedia_50341_apps.evozi.com.apk");
-        } else if (platform.equals("ios")) {
+        } else if (PLATFORM.equals("ios")) {
             capabilities.setCapability("platformName", "iOS");
             capabilities.setCapability("deviceName", "iPhone 8");
             capabilities.setCapability("platformVersion", "13.4");
-            capabilities.setCapability("app", "/Users/vitaliy/Documents/IdeaProjects/JavaAppiumAutomation/apks/Wikipedia.app");
+            capabilities.setCapability("app", "/Users/vitaliy/Desktop/Wikipedia.app");
         } else {
-            throw new Exception("Cannot get run platform from system environment " + platform);
+            throw new Exception("Cannot get run platform from system environment " + PLATFORM);
         }
         return capabilities;
     }
